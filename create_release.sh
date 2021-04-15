8439d686532bf392726e7088c7fd1e9401b2545c
nericksen <nericksen@paloaltonetworks.com>
2021-04-15 00:14:08 -0400

    Release MISP; Update release script

--- create_release.sh
+++ create_release.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-while read line; do git checkout develop -- Packs/$line; done < PACK_LIST.txt
+#while read line; do git checkout develop -- Packs/$line; done < PACK_LIST.txt
+
+while read line
+do
+    echo "Copying ${line} pack from develop branch"
+    git checkout develop -- "Packs/${line}"
+done < PACK_LIST.txt
+
+# Run through and generate dependecy tree
+# Checkout those packs as well
+# Create zip archive in directory on this branch under release version
